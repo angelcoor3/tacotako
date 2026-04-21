0cf8a7857b50ba8140fc06dc1f68e4152014c21e
angelcoor3 <angelcoor3@gmail.com>
2026-04-21 16:24:07 -0600

    web completa con menu y carrusel

--- js/app.js
+++ js/app.js
@@ -1,24 +1,35 @@
-// cargar componentes
-function cargarComponente(id, ruta) {
-  fetch(ruta)
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById(id).innerHTML = html;
+function cargarConfig() {
+  fetch("/config.json")
+    .then(res => res.json())
+    .then(data => {
+
+      const ahora = new Date();
+      const horaActual = ahora.getHours();
+      const minutosActual = ahora.getMinutes();
+
+      // convertir todo a minutos
+      const ahoraMin = horaActual * 60 + minutosActual;
+
+      const [hA, mA] = data.horario.apertura.split(":").map(Number);
+      const [hC, mC] = data.horario.cierre.split(":").map(Number);
+
+      const aperturaMin = hA * 60 + mA;
+      const cierreMin = hC * 60 + mC;
+
+      const estadoEl = document.getElementById("estado");
+
+      if (ahoraMin >= aperturaMin && ahoraMin < cierreMin) {
+        estadoEl.innerText = "🟢 ABIERTO";
+        estadoEl.className = "abierto";
+      } else {
+        estadoEl.innerText = "🔴 CERRADO";
+        estadoEl.className = "cerrado";
+      }
+
+      // horario
+      document.getElementById("horario").innerText = data.texto;
+
+      // logo
+      document.getElementById("logo").src = data.logo;
     });
 }
-
-// cargar vistas
-function cargarVista(nombre) {
-  fetch(`/views/${nombre}.html`)
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById("app").innerHTML = html;
-    });
-}
-
-// iniciar
-window.onload = () => {
-  cargarComponente("nav", "/components/navbar.html");
-  cargarComponente("footer", "/components/footer.html");
-  cargarVista("home");
-};
